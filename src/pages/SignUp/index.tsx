7c16e486f6ed2791cb73fb10b53b06508a4a8674
Linzmayer <lintzguigui1323@gmail.com>
2021-07-23 14:54:39 -0300

    refatoração e composição de componentes e estilização, aula dia 23-07

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,31 +1,45 @@
-import React from "react";
-import { FiUser, FiLock, FiArrowLeft } from 'react-icons/fi';
+import React, { useRef, useCallback } from "react";
+import { FiArrowLeft, FiUser, FiLock } from "react-icons/fi";
+import { Form } from "@unform/web";
+import { FormHandles } from "@unform/core";
 
 import Input from "../../components/Input";
 import Button from "../../components/Button";
 
-import { Container, Content, Background } from './styles'
+import { Container, Content, Background } from "./styles";
 
-const SignUp: React.FC = () => (
+const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback((data: object): void => {
+    console.log(data);
+  }, []);
+
+  return (
     <Container>
-        <Background/>
-        <Content>
-            <form>
-                <h1>Faça seu cadastro</h1>
+      <Background />
+      <Content>
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <h1>Faça seu cadastro</h1>
 
-                <Input icon={FiUser} name="email" placeholder="E-mail"/>
-                <Input icon={FiLock} name="senha" type="password" placeholder="Senha"/>
+          <Input icon={FiUser} name="email" placeholder="E-mail" />
+          <Input
+            icon={FiLock}
+            name="senha"
+            type="password"
+            placeholder="Senha"
+          />
 
-                <Button type="submit">Cadastrar</Button>
+          <Button type="submit">Cadastrar</Button>
+        </Form>
 
-            </form>
-
-            <a href="teste">
-                <FiArrowLeft/>
-                Voltar para o login</a>
-
-        </Content>
+        <a href="teste">
+          <FiArrowLeft />
+          Voltar para o login
+        </a>
+      </Content>
     </Container>
-);
+  );
+};
 
 export default SignUp;
--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,32 +1,32 @@
 import React from "react";
-import {FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from "react-icons/fi";
 
 import Input from "../../components/Input";
 import Button from "../../components/Button";
 
-import { Container, Content, Background } from './styles'
+import { Container, Content, Background } from "./styles";
 
 const SignIn: React.FC = () => (
-    <Container>
-        <Content>
-            <form>
-                <h1>Faça seu login</h1>
+  <Container>
+    <Content>
+      <form>
+        <h1>Faça seu login</h1>
 
-                <Input icon={FiMail} name="email" placeholder="E-mail"/>
-                <Input icon={FiLock} name="senha" type="password" placeholder="Senha"/>
+        <Input icon={FiMail} name="email" placeholder="E-mail" />
+        <Input icon={FiLock} name="senha" type="password" placeholder="Senha" />
 
-                <Button type="submit">Entrar</Button>
+        <Button type="submit">Entrar</Button>
 
-                <a href="teste">Esqueci minha senha</a>
-            </form>
+        <a href="teste">Esqueci minha senha</a>
+      </form>
 
-            <a href="teste">
-                <FiLogIn/>
-                Criar conta</a>
-
-        </Content>
-        <Background/>
-    </Container>
+      <a href="teste">
+        <FiLogIn />
+        Criar conta
+      </a>
+    </Content>
+    <Background />
+  </Container>
 );
 
 export default SignIn;
